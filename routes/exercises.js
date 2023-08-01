98988db9b423fcd727306d575091deb4ca2b41a5
Aw3som3ss <jabwunza@gmail.com>
2023-08-01 17:01:37 +0300

    backend

--- routes/exercises.js
+++ routes/exercises.js
@@ -3,27 +3,27 @@ let Exercise = require('../models/exercise.models');
 
 router.route('/').get((req, res) => {
 	Exercise.find()
-		.then(exercise => res.json(exercises))
-		.catch(err => res.status(400).json('Error: ' + err));
+		.then((exercise) => res.json(exercise))
+		.catch((err) => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
 	const username = req.body.username;
-    const description = req.body.description;
-    const duration = Number(req.body.duration);
-    const date = Date.parse(req.body.date);
+	const description = req.body.description;
+	const duration = Number(req.body.duration);
+	const date = Date.parse(req.body.date);
 
 	const newExercise = new Exercise({
-        username,
-        description,
-        duration,
-        date,
-    });
+		username,
+		description,
+		duration,
+		date,
+	});
 
 	newExercise
 		.save()
 		.then(() => res.json('Exercise succesfully added!'))
-		.catch(err => res.status(400).json('Error: ' + err));
+		.catch((err) => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
