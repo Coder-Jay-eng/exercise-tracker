bb0d256ae2aa1401aa4fb294d273f48acb326884
Aw3som3ss <jabwunza@gmail.com>
2023-08-01 15:41:35 +0300

    backend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 // environment variable
 require('dotenv').config();
@@ -12,6 +13,22 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+//Database connection
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {});
+const connection = mongoose.connection;
+connection.once('open', () => {
+	console.log('MongoDB database connection established successfully');
+});
+
+// Require the files
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+//Use the files
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 // starts server
 app.listen(port, () => {
