e8d034b5736f3d9d8e06bd92becc429d6ca9dccd
Aw3som3ss <jabwunza@gmail.com>
2023-08-08 15:46:49 +0300

    create exercise component

---
+++ src/components/create-user.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateUsers extends Component {
+	render() {
+		return (
+			<div>
+				<p>Congratulations, You are on the Create Users Component!</p>
+			</div>
+		);
+	}
+}
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ExercisesList extends Component {
+	render() {
+		return (
+			<div>
+				<p>Congratulations, You are on the Exercises List Component!</p>
+			</div>
+		);
+	}
+}
